97fbce7c319cec694b48e8198fca6ed863621f56
“dirkjan” <dirkjan96@gmail.com>
2015-05-11 14:05:17 +0200

    Smoke.. kunda

--- TankWars3000/TankWars3000/Game1.cs
+++ TankWars3000/TankWars3000/Game1.cs
@@ -32,10 +32,10 @@ namespace TankWars3000
 
         Lobby lobby;
 
-        Rectangle screenRec;
+        static Rectangle screenRec;
         static public Rectangle ScreenRec
         {
-            get { return ScreenRec; }
+            get { return screenRec; }
         }
 
         public Game1()
@@ -52,6 +52,8 @@ namespace TankWars3000
             tank =new Tank();
 
             gameState = GameStates.Lobby;
+
+            screenRec = new Rectangle(0, 0, graphics.GraphicsDevice.Viewport.Width, graphics.GraphicsDevice.Viewport.Height);
 
             base.Initialize();
         }
@@ -81,7 +83,7 @@ namespace TankWars3000
 
             if (gameState == GameStates.Lobby)
             {
-                // lobby code here Dirkjan
+                lobby.Update();
             }
             if (gameState == GameStates.Ingame)
             {
@@ -98,7 +100,7 @@ namespace TankWars3000
         }
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
              if (gameState == GameStates.Lobby)
                 {
--- TankWars3000/TankWars3000/Lobby/LobbyBackground.cs
+++ TankWars3000/TankWars3000/Lobby/LobbyBackground.cs
@@ -12,20 +12,42 @@ namespace TankWars3000
     class LobbyBackground
     {
         Texture2D smoketx;
+        Rectangle f1, f2, r1, r2;
 
         public LobbyBackground(ContentManager content)
         {
-            smoketx = content.Load<Texture2D>("images/smoke");
+            smoketx = content.Load<Texture2D>("images/smokee");
+
+            f1 = new Rectangle(0, 0, Game1.ScreenRec.Width, Game1.ScreenRec.Height);
+            f2 = new Rectangle(-Game1.ScreenRec.Width, 0, Game1.ScreenRec.Width, Game1.ScreenRec.Height);
+            r1 = new Rectangle(0, 0, Game1.ScreenRec.Width, Game1.ScreenRec.Height);
+            r2 = new Rectangle(Game1.ScreenRec.Width, 0, Game1.ScreenRec.Width, Game1.ScreenRec.Height);
         }
 
         public void Update()
         {
-            
+            if (f1.X > Game1.ScreenRec.Width)
+                f1.X = -Game1.ScreenRec.Width;
+            if (f2.X > Game1.ScreenRec.Width)
+                f2.X = -Game1.ScreenRec.Width;
+
+            if (r1.X < -Game1.ScreenRec.Width)
+                r1.X = Game1.ScreenRec.Width;
+            if (r2.X < -Game1.ScreenRec.Width)
+                r2.X = Game1.ScreenRec.Width;
+
+            f1.X += 1;
+            f2.X += 1;
+            r1.X -= 1;
+            r2.X -= 1;
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            
+            spriteBatch.Draw(smoketx, f1, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(smoketx, f2, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(smoketx, r1, null, Color.White, 0, Vector2.Zero, SpriteEffects.FlipHorizontally, 1);
+            spriteBatch.Draw(smoketx, r2, null, Color.White, 0, Vector2.Zero, SpriteEffects.FlipHorizontally, 1);
         }
     }
 }
