ba0a40fb6c475f8c7477f49f09dd9fe84ee4e5b5
Magnus <magnus.0565@gmail.com>
2015-05-11 14:09:10 +0200

    Formatering

--- TankWars3000/TankWars3000/Tank.cs
+++ TankWars3000/TankWars3000/Tank.cs
@@ -45,7 +45,7 @@ namespace TankWars3000
         public void Update(KeyboardState key)
         {
             if (key.IsKeyDown(Keys.W))
-        {
+            {
                 position += direction * speed;
             }
 
@@ -58,9 +58,9 @@ namespace TankWars3000
                 degrees += 6f;
                 direction.X = (float)Math.Cos(degrees);
                 direction.Y = (float)Math.Sin(degrees);
-        }
+            }
             if (key.IsKeyDown(Keys.A)/* && old.IsKeyUp(Keys.A)*/)
-        {
+            {
                 degrees -= 6f;
                 direction.X = (float)Math.Cos(degrees);
                 direction.Y = (float)Math.Sin(degrees);
