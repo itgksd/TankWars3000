8c8775b601942c790af398a5b4df41be4bc12219
Wietze <wietze.schelhaas12@itgksd.se>
2015-05-11 11:08:14 +0200

    Server Tank class

---
+++ TankWars3000/TankWars3000/Tank.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TankWars3000
+{
+    class Tank
+    {
+        int health;
+        int damage;
+
+        float direction;
+        TimeSpan bulletRate;
+
+        Vector2 speed;
+        Vector2 pos;
+        Vector2 spawnPos;
+
+        String Name;
+        Rectangle rect;
+
+        bool alive;
+        bool fired;
+
+        public void Update()
+        {
+            
+        }
+
+        private void CheckCollision()
+        {
+
+        }
+    }
+}
