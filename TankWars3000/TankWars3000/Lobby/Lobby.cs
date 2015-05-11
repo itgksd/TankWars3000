1b13a8271f6c6c5a5101603385ed1b56365377fa
“dirkjan” <dirkjan96@gmail.com>
2015-05-11 12:18:11 +0200

    Skapelse av lobby klassen

---
+++ TankWars3000/TankWars3000/Lobby/Lobby.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TankWars3000.Lobby
+{
+    class Lobby
+    {
+        public Lobby()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
