345cbee9deae1489601cf867f294b90b6f27a865
BuildTools <unconfigured@null.spigotmc.org>
2015-05-16 02:34:18 +0200

    Liten ändring i bakgrunden för jag har tråkigt

--- TankWars3000/TankWars3000/Lobby/LobbyBackground.cs
+++ TankWars3000/TankWars3000/Lobby/LobbyBackground.cs
@@ -12,43 +12,65 @@ namespace TankWars3000
     class LobbyBackground
     {
         Texture2D smoketx, backGlow;
-        Rectangle f1, f2, r1, r2;
+        //Rectangle f1, f2, r1, r2;
+        Video video;
+        VideoPlayer player;
+        Texture2D videoTexture;
 
         public LobbyBackground(ContentManager content)
         {
-            smoketx = content.Load<Texture2D>("images/smokee");
+            //smoketx = content.Load<Texture2D>("images/smokee");
             backGlow = content.Load<Texture2D>("images/backGlow");
 
-            f1 = new Rectangle(0, 0, Game1.ScreenRec.Width, Game1.ScreenRec.Height);
-            f2 = new Rectangle(-Game1.ScreenRec.Width, 0, Game1.ScreenRec.Width, Game1.ScreenRec.Height);
-            r1 = new Rectangle(0, 0, Game1.ScreenRec.Width, Game1.ScreenRec.Height);
-            r2 = new Rectangle(Game1.ScreenRec.Width, 0, Game1.ScreenRec.Width, Game1.ScreenRec.Height);
+            //f1 = new Rectangle(0, 0, Game1.ScreenRec.Width, Game1.ScreenRec.Height);
+            //f2 = new Rectangle(-Game1.ScreenRec.Width, 0, Game1.ScreenRec.Width, Game1.ScreenRec.Height);
+            //r1 = new Rectangle(0, 0, Game1.ScreenRec.Width, Game1.ScreenRec.Height);
+            //r2 = new Rectangle(Game1.ScreenRec.Width, 0, Game1.ScreenRec.Width, Game1.ScreenRec.Height);
+
+            video = content.Load<Video>("smoke_1_1");
+            player = new VideoPlayer();
         }
 
         public void Update()
         {
-            if (f1.X > Game1.ScreenRec.Width)
-                f1.X = -Game1.ScreenRec.Width;
-            if (f2.X > Game1.ScreenRec.Width)
-                f2.X = -Game1.ScreenRec.Width;
+            //if (f1.X > Game1.ScreenRec.Width)
+            //    f1.X = -Game1.ScreenRec.Width;
+            //if (f2.X > Game1.ScreenRec.Width)
+            //    f2.X = -Game1.ScreenRec.Width;
 
-            if (r1.X < -Game1.ScreenRec.Width)
-                r1.X = Game1.ScreenRec.Width;
-            if (r2.X < -Game1.ScreenRec.Width)
-                r2.X = Game1.ScreenRec.Width;
+            //if (r1.X < -Game1.ScreenRec.Width)
+            //    r1.X = Game1.ScreenRec.Width;
+            //if (r2.X < -Game1.ScreenRec.Width)
+            //    r2.X = Game1.ScreenRec.Width;
 
-            f1.X += 1;
-            f2.X += 1;
-            r1.X -= 1;
-            r2.X -= 1;
+            //f1.X += 1;
+            //f2.X += 1;
+            //r1.X -= 1;
+            //r2.X -= 1;
+
+
+            if (player.State == MediaState.Stopped)
+            {
+                player.IsLooped = true;
+                player.Play(video);
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(smoketx, f1, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 1);
-            spriteBatch.Draw(smoketx, f2, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 1);
-            spriteBatch.Draw(smoketx, r1, null, Color.White, 0, Vector2.Zero, SpriteEffects.FlipVertically, 1);
-            spriteBatch.Draw(smoketx, r2, null, Color.White, 0, Vector2.Zero, SpriteEffects.FlipVertically, 1);
+            //spriteBatch.Draw(smoketx, f1, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 1);
+            //spriteBatch.Draw(smoketx, f2, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 1);
+            //spriteBatch.Draw(smoketx, r1, null, Color.White, 0, Vector2.Zero, SpriteEffects.FlipVertically, 1);
+            //spriteBatch.Draw(smoketx, r2, null, Color.White, 0, Vector2.Zero, SpriteEffects.FlipVertically, 1);
+
+            // Only call GetTexture if a video is playing or paused
+            if (player.State != MediaState.Stopped)
+                videoTexture = player.GetTexture();
+            // Draw the video, if we have a texture to draw.
+            if (videoTexture != null)
+            {
+                spriteBatch.Draw(videoTexture, new Rectangle(0, 0, Game1.ScreenRec.Width, Game1.ScreenRec.Height + 2), Color.White);
+            }
 
             spriteBatch.Draw(backGlow, Game1.ScreenRec, Color.White);
         }
