8b15471303abff84fdfd6a7980fa2e326e73b6f6
emirr <emil.persson12@itgksd.se>
2015-05-22 15:39:38 +0200

    fixed foreach in game1

--- TankWars3000/TankWars3000/Game1.cs
+++ TankWars3000/TankWars3000/Game1.cs
@@ -104,12 +104,8 @@ namespace TankWars3000
             if (gameState == GameStates.Ingame)
             {
                 // The player
-                foreach (Tank tank in tanks)
-                {
                     tank.Update(graphicss);
                     tank.Input(input, Content);
-                }
-                
             }
             if (gameState == GameStates.Scoreboard)
             {
