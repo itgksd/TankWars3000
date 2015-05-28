453103c4ec635da51daaf339a39b53830b5fb96d
emirr <emil.persson12@itgksd.se>
2015-05-28 12:33:52 +0200

    don't do this kids

---
+++ TankWars3000/TankWars3000/TankTrack.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TankWars3000
+{
+    class TankTrack
+    {
+        Texture2D texture;
+
+        Vector2 position;
+
+        float alpha;
+
+        public void Update()
+        {
+            alpha -= 0.001f;
+        }
+
+        public TankTrack(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("Tank/Trail");
+        }
+    }
+}
