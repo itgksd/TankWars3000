7221c7bf3f415d60d18a561770d87b84e0d37090
“dirkjan” <dirkjan96@gmail.com>
2015-05-29 15:39:11 +0200

    Grejer till scoreboard

--- TankWars3000/TankWars3000/ScoreBoard.cs
+++ TankWars3000/TankWars3000/ScoreBoard.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Lidgren.Network;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -7,6 +12,64 @@ namespace TankWars3000
 {
     class ScoreBoard
     {
+        List<ScoreBoardItem> scoreBoardItems;
+        ContentManager content;
 
+        public ScoreBoard(ContentManager content)
+        {
+            this.content = content;
+            scoreBoardItems = new List<ScoreBoardItem>();
+        }
+
+        public void Update()
+        {
+            NetIncomingMessage incom; // MEssage that will contain the message comming from the server
+            if ((incom = Game1.Client.ReadMessage()) != null) // Are there any new messanges?
+            {
+                if (incom.MessageType == NetIncomingMessageType.Data) // Is it a "data" message?
+                {
+                    switch (incom.ReadByte())
+                    {
+                        case (byte)PacketTypes.FINALSCOREBOARD:
+                            // Packet contains:
+                            /// PacketType
+                            /// Player Amount \/ loop
+                            ///   Player Name
+                            ///   Player Kills
+                            ///   Player Deaths
+                            ///   Player Color R
+                            ///   Player Color G
+                            ///   Player Color B
+
+                            for (int i = 0; i < incom.ReadInt32(); i++)
+                            {
+                                Vector2 pos;
+                                if (scoreBoardItems.Count > 0)
+                                    pos = new Vector2(50, scoreBoardItems[scoreBoardItems.Count - 1].Position.Y + scoreBoardItems[scoreBoardItems.Count - 1].Height + 10);
+                                else
+                                    pos = new Vector2(50, 50);
+
+                                string name = incom.ReadString();
+                                int kills   = incom.ReadInt32();
+                                int deaths  = incom.ReadInt32();
+                                Color color = new Color(incom.ReadByte(), incom.ReadByte(), incom.ReadByte());
+                                int score = kills - deaths;
+
+                                scoreBoardItems.Add(new ScoreBoardItem(content, pos, kills, deaths, i, score, name, color));
+                            }
+
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            scoreBoardItems.ForEach(s => s.Draw(spriteBatch));
+        }
     }
 }
--- TankWars3000/TankWars3000/ScoreBoardItem.cs
+++ TankWars3000/TankWars3000/ScoreBoardItem.cs
@@ -11,27 +11,81 @@ namespace TankWars3000
     class ScoreBoardItem
     {
         static Texture2D pixel;
-        Rectangle posRect, playerRect, tankRect, scoreRect, killsRect, deathsRect;
+        Rectangle posRect, leftLineRect, playerRect, tankRect, scoreRect, killsRect, deathsRect;
         int kills, death, score, pos;
         string name;
         Color color;
-        float percentWidth, percentHeight;
         static SpriteFont font;
+        Vector2 scorePos, playerPos, posPos, killsPos, deathsPos, position;
 
-        public ScoreBoardItem (Vector2 position, int kills, int death, int pos, int score, string name, Color color, ContentManager content)
+        public Vector2 Position
         {
-            this.kills = kills;
-            this.death = death;
-            this.score = score;
-            this.pos = pos;
-            this.name = name;
-            this.color = color;
+            get { return position; }
+        }
+        public int Height
+        {
+            get { return posRect.Height; }
+        }
 
+        public ScoreBoardItem (ContentManager content, Vector2 position, int kills, int death, int pos, int score, string name, Color color)
+        {
+            this.position = position;
+            this.kills    = kills;
+            this.death    = death;
+            this.score    = score;
+            this.pos      = pos;
+            this.name     = name;
+            this.color    = color;
+
+            // Resources
+            if (pixel == null || font == null)
+            {
+                pixel = content.Load<Texture2D>("pixel");
+                font = content.Load<SpriteFont>("DefFont");
+            }
+
+
+            // Rectangles
             int height = 50;
-            posRect = new Rectangle((int)position.X, (int)position.Y, 136, height);
-            //playerRect = new Rectangle(
+            posRect      = new Rectangle((int)position.X, (int)position.Y, 136, height);
+            leftLineRect = new Rectangle(posRect.X, posRect.Y, 4, posRect.Height);
+            playerRect   = new Rectangle(posRect.X + posRect.Width + 10, (int)position.Y, 500, height);
+            tankRect     = new Rectangle(0,0,10,10); // Ändra senare
+            scoreRect    = new Rectangle(playerRect.X + playerRect.Width + 10, playerRect.Y, 60, height);
+            killsRect    = new Rectangle(scoreRect.X + scoreRect.Width + 10, scoreRect.Y, 60, height);
+            deathsRect   = new Rectangle(killsRect.X + killsRect.Width + 10, killsRect.Y, 60, height);
 
+            // Text positions
+            posPos    = new Vector2(posRect.X + 6, posRect.Y);
+            playerPos = new Vector2(playerRect.X + 50, playerRect.Y);
+            scorePos  = new Vector2(scoreRect.X + 5, scoreRect.Y);
+            killsPos  = new Vector2(killsRect.X + 5, killsRect.Y);
+            deathsPos = new Vector2(deathsRect.X + 5, deathsRect.Y);
+        }
 
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Position
+            spriteBatch.Draw(pixel, posRect, Color.White);
+            spriteBatch.Draw(pixel, leftLineRect, Color.Blue);
+            spriteBatch.DrawString(font, ""+score, posPos, Color.White);
+
+            // Player
+            spriteBatch.Draw(pixel, playerRect, Color.White);
+            // Draw tank
+            // Draw name
+
+            // Score
+            spriteBatch.Draw(pixel, scoreRect, Color.White);
+            // Draw score
+
+            // Kills
+            spriteBatch.Draw(pixel, killsRect, Color.White);
+            // Draw kills
+
+            // Deaths
+            spriteBatch.Draw(pixel, deathsRect, Color.White);
+            // Draw deaths
         }
     }
 }
