999bc99c685c5f55c8a0c86a6446cae0d43544ac
Magnus <magnus.0565@gmail.com>
2015-05-29 10:55:59 +0200

    incmsg_name

--- TankWars3000/TankWars3000/Tank_startpos.cs
+++ TankWars3000/TankWars3000/Tank_startpos.cs
@@ -9,17 +9,18 @@ namespace TankWars3000
 {
     class Tank_startpos
     {
+        string incmsg_name;
         public void Update(NetIncomingMessage incmsg, List<Tank> tanks)
         {
                 if ((incmsg = Game1.Client.ReadMessage()) != null)
                 {
                     if (incmsg.ReadByte() == (byte)PacketTypes.STARTPOS)
                     {
+                        incmsg_name =incmsg.ReadString();
                         for (int i = 0; i < tanks.Count; i++ )
                         {
-                            if (tanks[i].Name == incmsg.ReadString())
+                            if (tanks[i].Name == incmsg_name)
                             {
-                                tanks[i].Name = incmsg.ReadString();
                                 tanks[i].Angle = incmsg.ReadFloat();
                                 tanks[i].Position = new Vector2(incmsg.ReadInt32(), incmsg.ReadInt32());
                             }
